f763d992c05f9bea879eedf4815c84034b278e16
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-05-11 21:35:46 -0400

    Created knowingly workiing functions

--- main.d
+++ main.d
@@ -1,5 +1,22 @@
 import std.stdio;
+//import std.variant;alias var = Variant;
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Might need this.
+int[int[2]] clean;
 int[int[2]] current_board;
 int[int[2]] sum_board;
 int[int[2]] next_board;
+void display()
+{
+  foreach(key,value;current_board)
+  {
+    write("(");write(key[0]);write(",");write(key[1]);write(")=>");writeln(value);
+  }
+}
+void clear()
+{
+  current_board=next_board;
+  next_board=clean.dup;
+  sum_board=clean.dup;
+}
 void main(){}
+
